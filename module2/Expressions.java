3813dcf670412b214aad73bb3ddac8d42223b576
jfails <jerryfails@boisestate.edu>
2025-09-18 14:33:21 -0600

    added an expression example

---
+++ module2/Expressions.java
@@ -0,0 +1,13 @@
+public class Expressions {
+
+    public static void main(String[] args) {
+        
+        int numCookies = 12;
+        int numFriends = 5;
+
+        int total = (int)((double)numCookies / numFriends);
+
+        System.out.println("Each person gets: " + total);
+
+    }
+}
