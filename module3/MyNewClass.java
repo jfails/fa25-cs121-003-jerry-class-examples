cb807942e6f6d5c8dd91c132126653eefe7f95ca
jfails <jerryfails@boisestate.edu>
2025-09-23 15:59:21 -0600

    added a blank class example (MyNewClass)

---
+++ module3/MyNewClass.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+/**
+ * This class does ....
+ * @author Jerry
+ */
+public class MyNewClass 
+{
+    
+    public static void main(String[] args) 
+    {
+        
+    }
+}
