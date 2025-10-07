43af5d3d519aa426777649e5f15d843c52f395c7
jfails <jerryfails@boisestate.edu>
2025-10-07 15:33:13 -0600

    added comments to code

--- module4/LoopExample.java
+++ module4/LoopExample.java
@@ -1,30 +1,36 @@
 import java.util.Scanner;
 
 /**
- * Shows how to do some things with loops and characters
+ * Shows how to do some things with loops and characters.
+ * @author Jerry Fails
  */
 public class LoopExample {
 
 
     public static void main(String[] args) {
     
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print("Choose an option, q to quit: ");
-        char c = '\u1E0B'; //keyboard.nextLine().charAt(0);
+        // EXAMPLE of sentinel value
 
+        // Scanner keyboard = new Scanner(System.in);
+        // System.out.print("Choose an option, q to quit: ");
+        // char c = keyboard.nextLine().charAt(0);
         // while (!(c == 'q' || c == 'Q'))
         // {
         //     System.out.print("Choose an option, q to quit: ");
         //     c = keyboard.nextLine().charAt(0);
         // }
+        // // always remember to close your Scanner
+        // keyboard.close();
 
+        // EXAMPLE: loop to show characters (and simple character math)
+        // setting up variables
+        char c = '\u1E0B'; //keyboard.nextLine().charAt(0);
         while (c < 10000)
         {
             System.out.print(c + " ");
             c++;
         }
 
-        keyboard.close();
     }
     
 }
