5ffc84b7ef8b7dc4b8b4d7c6d2822aa55e593adb
jfails <jerryfails@boisestate.edu>
2025-10-16 15:27:36 -0600

    added a csv arraylist example

--- module5/FileExample.java
+++ module5/FileExample.java
@@ -1,33 +1,48 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class FileExample {
     public static void main(String[] args) {
-        
+        // a file to read from
         File f = new File("data.txt");
 
         try
         {
             Scanner fileScan = new Scanner(f);
+            ArrayList<Integer> nums = new ArrayList<Integer>();
 
+            // while there is still something in the file
             while (fileScan.hasNextLine())
             {
-                String line = fileScan.nextLine();
-                Scanner lineScanner = new Scanner(line);
-                lineScanner.useDelimiter(",");
-                while (lineScanner.hasNext())
+                String line = fileScan.nextLine();          // get the next line
+                Scanner lineScanner = new Scanner(line);    // setup a scanner to read from it
+                lineScanner.useDelimiter(",");              // setup the delimiter (to read commas)
+                while (lineScanner.hasNext())               // while there is something in the line
                 {
-                    System.out.println(lineScanner.next());
+                    nums.add(Integer.parseInt(lineScanner.next()));
+                    //System.out.print(lineScanner.next() + ";"); // get the next part
                 }
-                lineScanner.close();
+                System.out.println();
+                lineScanner.close();                        // clean up and close the line scanner
             }
-            fileScan.close();
+            fileScan.close();                               // clean up and close the file scanner
+
+
+            // print out all of the numbers in my collection
+            for (Integer num : nums)
+            {
+                System.out.print(num + " ");
+            }
         }
         catch (FileNotFoundException fnfe)
         {
             System.out.println("File not found!");
             fnfe.printStackTrace();
         }
+
+
+        
     }
 }
---
+++ module5/ReadCSV.java
@@ -0,0 +1,9 @@
+/**
+ * Read info from a CSV file ...
+ * @author Jerry Fails
+ */
+public class ReadCSV {
+    public static void main(String[] args) {
+        
+    }
+}
