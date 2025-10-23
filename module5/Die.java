6de9a4a5fdb8e363e77a8e3256c0bf88468e83b6
jfails <jerryfails@boisestate.edu>
2025-10-23 16:10:13 -0600

    Adding a Die class example

---
+++ module5/Die.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+
+/**
+ * A Die class that represents a multi-faced die and it's current face value.
+ * @author Jerry Fails
+ */
+public class Die 
+{
+    // FIELDS
+    private int numSides;
+    private int faceValue;
+    private Random randomGen;
+
+    // CONSTRUCTORS
+    /**
+     * Default constructor sets the number of sides to 6, and randomly sets the face value.
+     */
+    public Die()
+    { 
+        this.numSides = 6;
+        this.randomGen = new Random();
+        this.roll();
+    }
+
+    /**
+     * Constructor to create a mult-sided die
+     * @param numSides the number of sides this die will have
+     */
+    public Die(int numSides)
+    {
+        this.numSides = numSides;
+        this.randomGen = new Random();
+        this.roll();
+    }
+
+    // METHODS
+
+    /**
+     * Rolls the die and returns the value.
+     * @return the new value after being rolled
+     */
+    public int roll()
+    {
+        faceValue = randomGen.nextInt(numSides) + 1;
+        return faceValue;
+    }
+
+    public String toString()
+    {
+        return "Die[" + numSides + "]: " + faceValue;
+    }
+
+    public static void main(String[] args) 
+    {
+        Die die1 = new Die();
+        Die die2 = new Die(13);
+        
+
+        for (int i = 0; i < 10; i++)
+        {
+            die1.roll();
+            die2.roll();
+            System.out.print("die1: " + die1 + "\t");
+            System.out.println("die1: " + die2);
+        }
+    }    
+}
--- module5/FileExample.java
+++ module5/FileExample.java
@@ -42,7 +42,6 @@ public class FileExample {
             fnfe.printStackTrace();
         }
 
-
         
     }
 }
