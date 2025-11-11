bb9da91d7aa2183242150302f8bc995a98e55cd9
jfails <jerryfails@boisestate.edu>
2025-11-11 15:35:17 -0700

    added documentation

--- module7/ReverseOrder.java
+++ module7/ReverseOrder.java
@@ -1,14 +1,21 @@
+/**
+ * An example to show reversing the order of things in an array.
+ * @author Jerry Fails
+ */
 public class ReverseOrder {
     public static void main(String[] args) {
         
+        // create an array
         int[] nums = {1, 2, 3, 4, 5};
 
+        // print out the array
         System.out.println(nums);
         for (int i : nums)
         {
             System.out.print(i + " ");
         }
 
+        // swap the elements in the array
         for (int i = 0; i < nums.length / 2; i++)
         {
             int x = nums[i];
@@ -16,6 +23,7 @@ public class ReverseOrder {
             nums[nums.length - i - 1] = x;
         }
 
+        // print out the array
         System.out.println(nums);
         for (int i : nums)
         {
